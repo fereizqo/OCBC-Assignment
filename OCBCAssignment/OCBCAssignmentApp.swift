eccef96625b00b1b73123832d058ffe9c0c40a5e
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2022-05-23 10:42:28 +0700

    feat: initial commit

---
+++ OCBCAssignment/OCBCAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCBCAssignmentApp.swift
+//  OCBCAssignment
+//
+//  Created by Fereizqo Sulaiman on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct OCBCAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
